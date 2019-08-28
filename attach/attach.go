b7e7a6873a17ca725b5c4b74117058ba6b6df6c2
John Shenk <john.shenk@rackspace.com>
2019-08-28 10:10:01 -0500

    removes cruft; removes funcs

--- attach/attach.go
+++ attach/attach.go
@@ -8,18 +8,18 @@ import (
 )
 
 // AttachElements attaches a slice of elements to a js.Value
-func AttachElements(elements []element.Element, body js.Value, funcs map[string]js.Func) {
+func AttachElements(elements []element.Element, body js.Value) {
 	for _, e := range elements {
 		if e.Parent == nil {
-			AppendToValue(&e, body, funcs)
+			AppendToValue(&e, body)
 		} else {
-			Append(&e, e.Parent, funcs)
+			Append(&e, e.Parent)
 		}
 	}
 }
 
 // AppendToValue ...
-func AppendToValue(e *element.Element, v js.Value, funcs map[string]js.Func) {
+func AppendToValue(e *element.Element, v js.Value) {
 	element := js.Global().Get("document").Call("createElement", e.Tag)
 	text := js.Global().Get("document").Call("createTextNode", e.InnerHTML)
 	if e.GosxID == "" {
@@ -31,14 +31,13 @@ func AppendToValue(e *element.Element, v js.Value, funcs map[string]js.Func) {
 		element.Call("setAttribute", key, val)
 	}
 	for key, val := range e.Listeners {
-		// element.Call("addEventListener", strings.TrimRight(key, "on"), js.FuncOf(val))
 		element.Call("addEventListener", strings.TrimRight(key, "on"), val)
 	}
 	v.Call("appendChild", element)
 }
 
 // Append ...
-func Append(e *element.Element, elem *element.Element, funcs map[string]js.Func) {
+func Append(e *element.Element, elem *element.Element) {
 	parent := js.Global().Get("document").Call("getElementById", elem.GosxID)
-	AppendToValue(e, parent, funcs)
+	AppendToValue(e, parent)
 }
--- example/main.go
+++ example/main.go
@@ -17,40 +17,6 @@ func main() {
 
 	body := js.Global().Get("document").Get("body")
 
-	// div := &element.Element{
-	// 	Tag: "div",
-	// }
-	// attach.AppendToValue(div, body, nil)
-	//
-	// a := &element.Element{
-	// 	Tag:        "a",
-	// 	InnerHTML:  "cnn",
-	// 	Attributes: map[string]string{"href": "https://cnn.com"},
-	// }
-	// attach.Append(a, div, nil)
-	//
-	// button := js.Global().Get("document").Call("createElement", "div")
-	// text := js.Global().Get("document").Call("createTextNode", "BUTTON")
-	// button.Call("setAttribute", "id", "myButton") // set if not popuilated
-	// button.Call("appendChild", text)
-	// body.Call("appendChild", button)
-	// cb := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-	// 	fmt.Println("button clicked")
-	// 	return nil
-	// })
-	// js.Global().Get("document").Call("getElementById", "myButton").Call("addEventListener", "click", cb)
-	// // funcs := map[string]js.Func{"test": test}
-	//
-	// tags := `<div className="foobar">
-	// 	<a href="https://www.ebay.com" onClick="test">EBAY</a>
-	// 	<div onClick="test">CLICK</div>
-	// 	<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiTnm8jW-nmGGT08sRrxsPFiZQ7r5tI2IYIABNcS56JIbpJTl-" />
-	// </div>`
-	// elements, err := element.ParseElement(tags)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// attach.AttachElements(elements, body, funcs)
 	ctx, cf := context.WithCancel(context.Background())
 	components.YearSelect(body, ctx)
 	<-ch
